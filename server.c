7b8abcf82f023774f929ebff5248e089f50353b6
MatyasMa <masek.matyas@seznam.cz>
2025-01-13 13:28:20 +0100

    server connect to custom ip and port

--- server.c
+++ server.c
@@ -375,13 +375,33 @@ int create_shared_memory() {
 }
 
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <IP>:<port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-int main() {
+    char *ip_port = argv[1];
+    char *colon = strchr(ip_port, ':');
+    if (!colon) {
+        fprintf(stderr, "Invalid parameter format. Use <IP>:<port>.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Split IP and port
+    *colon = '\0';
+    const char *ip = ip_port;
+    int port = atoi(colon + 1);
+
+    if (port <= 0 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %d\n", port);
+        exit(EXIT_FAILURE);
+    }
+
     int server_fd, client_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    
 
     // Set up signal handler to reap zombie processes
     signal(SIGCHLD, handle_sigchld);
@@ -392,18 +412,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
+    // Forcefully attaching socket to the port
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
+    // Set up server address
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &address.sin_addr) <= 0) {
+        fprintf(stderr, "Invalid IP address: %s\n", ip);
+        exit(EXIT_FAILURE);
+    }
 
-    // Binding socket to the port
-    if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0) {
+    // Binding socket to the specified IP and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -413,7 +437,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Server started. Waiting for clients...\n");
+    printf("Server started on %s:%d. Waiting for clients...\n", ip, port);
 
     // Create shared memory for players
     if (create_shared_memory() == -1) {
@@ -421,8 +445,6 @@ int main() {
     }
 
     while (1) {
-        struct sockaddr_in address;
-        int addrlen = sizeof(address);
         // Accept new client connection
         if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
             perror("accept");
@@ -466,6 +488,101 @@ int main() {
     }
 
     close(server_fd);
-    
+
     return 0;
 }
+
+
+// int main() {
+//     int server_fd, client_socket;
+//     struct sockaddr_in address;
+//     int opt = 1;
+//     int addrlen = sizeof(address);
+    
+
+//     // Set up signal handler to reap zombie processes
+//     signal(SIGCHLD, handle_sigchld);
+
+//     // Creating socket file descriptor
+//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+//         perror("socket failed");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     // Forcefully attaching socket to the port 8080
+//     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+//         perror("setsockopt");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     address.sin_family = AF_INET;
+//     address.sin_addr.s_addr = INADDR_ANY;
+//     address.sin_port = htons(PORT);
+
+//     // Binding socket to the port
+//     if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0) {
+//         perror("bind failed");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (listen(server_fd, 3) < 0) {
+//         perror("listen");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     printf("Server started. Waiting for clients...\n");
+
+//     // Create shared memory for players
+//     if (create_shared_memory() == -1) {
+//         exit(EXIT_FAILURE);
+//     }
+
+//     while (1) {
+//         struct sockaddr_in address;
+//         int addrlen = sizeof(address);
+//         // Accept new client connection
+//         if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+//             perror("accept");
+//             exit(EXIT_FAILURE);
+//         }
+
+//         if (players_count < MAX_PLAYERS) {
+//             players[players_count].id = players_count + 1;
+//             players[players_count].socket_fd = client_socket;
+
+//             printf("Client connected: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+//             printf("Client %d connected with socket_fd %d.\n", players[players_count].id, client_socket);
+
+//             // Create unique player ID to pass to the thread
+//             int *player_id = malloc(sizeof(int));
+//             *player_id = players_count;
+
+//             // Create thread to handle client
+//             pthread_t thread_id;
+//             if (pthread_create(&thread_id, NULL, handle_client, (void *)player_id) != 0) {
+//                 perror("pthread_create failed");
+//                 close(client_socket);
+//                 continue;
+//             }
+
+//             players[players_count].thread = thread_id;
+
+//             // Increment player count
+//             players_count++;
+//         } else {
+//             // Max players reached, send error message to client
+//             char *error_message = "Max players reached. Try again later.\n";
+//             send(client_socket, error_message, strlen(error_message), 0);
+//             close(client_socket);
+//         }
+//     }
+
+//     // Wait for all threads to finish before closing the server
+//     for (int i = 0; i < MAX_PLAYERS; ++i) {
+//         pthread_join(players[i].thread, NULL);
+//     }
+
+//     close(server_fd);
+    
+//     return 0;
+// }
