788fa3f809b505fdfac3ea92c930856c58f85e47
MatyasMa <masek.matyas@seznam.cz>
2025-01-25 18:49:03 +0100

    happy day

--- server.c
+++ server.c
@@ -285,13 +285,6 @@ void start_clients() {
 }
 
 
-void get_first_players_cards() {
-    // for (int i = 0; i < MAX_PLAYERS; ++i) {
-    //     player_hit(i);
-    //     player_hit(i);
-    // }
-}
-
 void unready_to_play_hand_players() {
     for (int i = 0; i < MAX_PLAYERS; ++i) {
         players[i].has_first_cards = 0;
@@ -396,13 +389,6 @@ void broadcast_message(const char *message) {
 }
 
 int create_shared_memory() {
-    // // Dynamicky alokujeme paměť pro pole hráčů
-    // players = (struct player *)malloc(sizeof(struct player) * MAX_PLAYERS);
-    // if (players == NULL) {
-    //     perror("malloc failed");
-    //     return -1;
-    // }
-
     shmid = shmget(IPC_PRIVATE, sizeof(struct player) * MAX_PLAYERS, IPC_CREAT | 0666); 
     if (shmid == -1) {
         perror("shmget failed");
@@ -558,97 +544,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-// int main() {
-//     int server_fd, client_socket;
-//     struct sockaddr_in address;
-//     int opt = 1;
-//     int addrlen = sizeof(address);
-    
-
-//     // Set up signal handler to reap zombie processes
-//     signal(SIGCHLD, handle_sigchld);
-
-//     // Creating socket file descriptor
-//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-//         perror("socket failed");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     // Forcefully attaching socket to the port 8080
-//     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-//         perror("setsockopt");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     address.sin_family = AF_INET;
-//     address.sin_addr.s_addr = INADDR_ANY;
-//     address.sin_port = htons(PORT);
-
-//     // Binding socket to the port
-//     if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0) {
-//         perror("bind failed");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     if (listen(server_fd, 3) < 0) {
-//         perror("listen");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     printf("Server started. Waiting for clients...\n");
-
-//     // Create shared memory for players
-//     if (create_shared_memory() == -1) {
-//         exit(EXIT_FAILURE);
-//     }
-
-//     while (1) {
-//         struct sockaddr_in address;
-//         int addrlen = sizeof(address);
-//         // Accept new client connection
-//         if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-//             perror("accept");
-//             exit(EXIT_FAILURE);
-//         }
-
-//         if (players_count < MAX_PLAYERS) {
-//             players[players_count].id = players_count + 1;
-//             players[players_count].socket_fd = client_socket;
-
-//             printf("Client connected: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-//             printf("Client %d connected with socket_fd %d.\n", players[players_count].id, client_socket);
-
-//             // Create unique player ID to pass to the thread
-//             int *player_id = malloc(sizeof(int));
-//             *player_id = players_count;
-
-//             // Create thread to handle client
-//             pthread_t thread_id;
-//             if (pthread_create(&thread_id, NULL, handle_client, (void *)player_id) != 0) {
-//                 perror("pthread_create failed");
-//                 close(client_socket);
-//                 continue;
-//             }
-
-//             players[players_count].thread = thread_id;
-
-//             // Increment player count
-//             players_count++;
-//         } else {
-//             // Max players reached, send error message to client
-//             char *error_message = "Max players reached. Try again later.\n";
-//             send(client_socket, error_message, strlen(error_message), 0);
-//             close(client_socket);
-//         }
-//     }
-
-//     // Wait for all threads to finish before closing the server
-//     for (int i = 0; i < MAX_PLAYERS; ++i) {
-//         pthread_join(players[i].thread, NULL);
-//     }
-
-//     close(server_fd);
-    
-//     return 0;
-// }
